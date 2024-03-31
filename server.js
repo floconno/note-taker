055291ea6ff8a4bd85d9b8606c743d5cf99296b5
floconno <floconno@asu.edu>
2024-03-31 16:40:39 -0700

    set ups server.js and added uuid helper to generate unique ids

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
