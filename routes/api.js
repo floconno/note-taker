f2d634031e41183c4bf86e8bb98abaf607138d56
floconno <floconno@asu.edu>
2024-04-08 23:58:38 -0700

    working on routes

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const api = require('express').Router();
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+
+
+
+// GET /api/notes 
+app.get('/api/notes', function(req, res) {
+
+});
+
+// POST /api/notes
+
+
+
+
+
+
--- routes/html.js
+++ routes/html.js
@@ -1,1 +1,13 @@
-const express = require('express');
+const html = require('express').Router();
+const path = require('path');
+
+// GET /notes
+
+app.get('/notes', function(req, res) {
+
+});
+
+// GET *
+
+
+module.exports = html;
