c704ccd3aeccd4ee36c815742f2528b0ccf8a372
floconno <floconno@asu.edu>
2024-04-16 16:58:26 -0700

    added get request for notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,15 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
 
 
 
