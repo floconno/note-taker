b708a156aae360d06cbf464f8000d58a30ac1957
floconno <floconno@asu.edu>
2024-04-16 17:46:05 -0700

    fixed notes route and added post request

--- routes/notes.js
+++ routes/notes.js
@@ -9,6 +9,35 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// POST Route for submitting feedback
+notes.post('/', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to save a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text }= req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in saving note');
+  }
+});
 
 module.exports = notes;
 
